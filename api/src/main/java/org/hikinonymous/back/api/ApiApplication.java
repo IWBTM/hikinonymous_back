8cc1ee11807c706b3d30f3eb3ecf847ae21e747c
GANG <rnlduadl2000@naver.com>
2024-05-26 17:47:38 +0900

    entityScan Core path로 지정

--- api/src/main/java/org/hikinonymous/back/api/ApiApplication.java
+++ api/src/main/java/org/hikinonymous/back/api/ApiApplication.java
@@ -2,8 +2,10 @@ package org.hikinonymous.back.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-@SpringBootApplication
+@EntityScan(basePackages = "org.hikinonymous.back.core.entity")
+@SpringBootApplication(scanBasePackages = "org.hikinonymous.back.core")
 public class ApiApplication {
 
     public static void main(String[] args) {
--- cms/src/main/java/org/hikinonymous/back/cms/CmsApplication.java
+++ cms/src/main/java/org/hikinonymous/back/cms/CmsApplication.java
@@ -2,8 +2,10 @@ package org.hikinonymous.back.cms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-@SpringBootApplication
+@EntityScan(basePackages = "org.hikinonymous.back.core.entity")
+@SpringBootApplication(scanBasePackages = "org.hikinonymous.back.core")
 public class CmsApplication {
 
     public static void main(String[] args) {
