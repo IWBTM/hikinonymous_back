552544deceb64e70f5c30a4714185bf44acd94be
GANG <rnlduadl2000@naver.com>
2024-05-26 18:49:09 +0900

    GlobalExceptionHandler 추가

---
+++ core/src/main/java/org/hikinonymous/back/core/handler/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package org.hikinonymous.back.core.handler;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity handleException(Exception e) {
+        System.out.println("====================");
+        System.out.println("e.getCause():: " + e.getCause());
+        System.out.println("e.getMessage():: " + e.getMessage());
+        System.out.println("====================");
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+}
