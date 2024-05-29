c0647dff3ec50e16773a57c89c0b2c6764a174bb
김경민(화려한 덕후들) <gang00@g-oduk.com>
2024-05-29 15:39:33 +0900

    [CORE] HttpResponseStatus 추가

--- core/src/main/java/org/hikinonymous/back/core/dto/HttpResponseStatus.java
+++ core/src/main/java/org/hikinonymous/back/core/dto/HttpResponseStatus.java
@@ -3,6 +3,7 @@ package org.hikinonymous.back.core.dto;
 public enum HttpResponseStatus {
     
     SUCCESS(200, "성공"),
+    BAD_REQUEST(400, "잘못된 접근"),
     SERVER_ERROR(500, "서버 에러");
     
     private final Integer code;
