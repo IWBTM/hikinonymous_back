7b1fde36ca73f385828f86f22be1d4fe8ef25339
GANG <rnlduadl2000@naver.com>
2024-05-30 23:08:55 +0900

    [SQL] CODE MASTER 쿼리 추가

---
+++ core/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO tb_code_master (codeMasterSeq, codeMaster, codeMasterNm, delYn)
+VALUES (1, 'MANAGER_STATUS', '관리자 상태', 'N'),
+        (2, 'MENU_AUTH', '메뉴 권한', 'N'),
+        (3, 'DEPARTMENT', '부서', 'N'),
+        (4, 'POSITION', '직책', 'N'),
+        (5, 'MEMBER_STATUS', '회원 상태', 'N'),
+        (6, 'JOIN_TYPE', '가입 경로', 'N');
