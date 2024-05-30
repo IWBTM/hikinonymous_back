a991c053bef8a6ae2e257cc88dfd8dab301213bc
김경민(화려한 덕후들) <gang00@g-oduk.com>
2024-05-30 15:35:15 +0900

    [CORE] GlobalExceptionHandler Exception 추가, logger 추가

--- cms/src/main/java/org/hikinonymous/back/cms/controller/LoginController.java
+++ cms/src/main/java/org/hikinonymous/back/cms/controller/LoginController.java
@@ -1,17 +1,23 @@
 package org.hikinonymous.back.cms.controller;
 
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.hikinonymous.back.core.dto.LoginDto;
 import org.hikinonymous.back.core.dto.ResponseDto;
 import org.hikinonymous.back.core.utils.ResponseUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping(value = "/cms/login/")
 public class LoginController {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @PostMapping(value = "proc")
     public ResponseDto proc(
--- core/src/main/java/org/hikinonymous/back/core/handler/GlobalExceptionHandler.java
+++ core/src/main/java/org/hikinonymous/back/core/handler/GlobalExceptionHandler.java
@@ -3,8 +3,11 @@ package org.hikinonymous.back.core.handler;
 import org.hikinonymous.back.core.dto.HttpResponseStatus;
 import org.hikinonymous.back.core.dto.ResponseDto;
 import org.hikinonymous.back.core.utils.ResponseUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -12,28 +15,43 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity handleException(Exception e) {
-        System.out.println("====================");
-        System.out.println("e.getCause():: " + e.getCause());
-        System.out.println("e.getMessage():: " + e.getMessage());
-        System.out.println("====================");
+        logger.info("====================");
+        logger.info("e.getCause():: " + e.getCause());
+        logger.info("e.getMessage():: " + e.getMessage());
+        logger.info("====================");
         return new ResponseEntity(HttpStatus.OK);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseDto handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
-        System.out.println("========== S MethodArgumentNotValidException ==========");
+        logger.info("========== S MethodArgumentNotValidException ==========");
         ResponseDto responseDto = new ResponseDto();
-        ResponseUtil.validEmailForm(responseDto);
         String message = e.getMessage();
         String messageTemp;
         messageTemp = message.substring(message.lastIndexOf("[") + 1);
         messageTemp = messageTemp.substring(0, messageTemp.indexOf("]"));
-        if (message.contains("NotBlank")) messageTemp += "(은)는 필수값입니다.";
-        responseDto.setMessage(messageTemp);
-        System.out.println("========== E MethodArgumentNotValidException ==========");
+        
+        if (message.contains("NotBlank")) {
+            ResponseUtil.validRequestParameterForm(responseDto);
+            responseDto.setMessage(messageTemp + "(은)는 필수값입니다.");
+        } else {
+            ResponseUtil.emptyRequestParameter(responseDto);
+            responseDto.setMessage(messageTemp);
+        }
+        
+        logger.info("========== E MethodArgumentNotValidException ==========");
         return responseDto;
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseDto handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
+        logger.info("========== S HttpMessageNotReadableException ==========");
+        logger.info("========== E HttpMessageNotReadableException ==========");
+        return ResponseUtil.emptyRequestBody(new ResponseDto());
     }
 
 }
