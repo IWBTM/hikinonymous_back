46c400036c164e306d079d9ceca49e9565579847
GANG <rnlduadl2000@naver.com>
2024-05-30 23:07:56 +0900

    [CORE] CodeMasterEntity 추가

---
+++ core/src/main/java/org/hikinonymous/back/core/entity/CodeMasterEntity.java
@@ -0,0 +1,41 @@
+package org.hikinonymous.back.core.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.Comment;
+import org.hibernate.annotations.DynamicInsert;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity(name = "tb_code_master")
+@DynamicInsert
+public class CodeMasterEntity {
+
+    // 코드 SEQ
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Comment("코드 마스터 SEQ")
+    private Long codeMasterSeq;
+
+    // 코드
+    @Column(nullable = false, length = 50)
+    @Comment("코드")
+    private String codeMaster;
+
+    // 코드 이름
+    @Column(nullable = false, length = 50)
+    @Comment("코드 이름")
+    private String codeMasterNm;
+
+    // 삭제 여부
+    @Column(nullable = false, length = 1)
+    @Comment("삭제 여부")
+    @ColumnDefault(value = "'N'")
+    private String delYn;
+}
