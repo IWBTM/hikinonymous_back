678730b78ed540959dafbacc3f54829dd9883d3f
김경민(화려한 덕후들) <gang00@g-oduk.com>
2024-05-31 14:28:27 +0900

    [CMS] Login 성공 시 JWT 추가

--- cms/src/main/java/org/hikinonymous/back/cms/controller/LoginController.java
+++ cms/src/main/java/org/hikinonymous/back/cms/controller/LoginController.java
@@ -1,13 +1,17 @@
 package org.hikinonymous.back.cms.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.hikinonymous.back.core.dto.LoginDto;
+import org.hikinonymous.back.core.dto.ManagerDto;
 import org.hikinonymous.back.core.dto.ResponseDto;
 import org.hikinonymous.back.core.entity.ManagerEntity;
 import org.hikinonymous.back.core.service.ManagerService;
 import org.hikinonymous.back.core.utils.CommonUtil;
+import org.hikinonymous.back.core.utils.EncUtil;
+import org.hikinonymous.back.core.utils.JwtUtil;
 import org.hikinonymous.back.core.utils.ResponseUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ServerErrorException;
 
+import java.util.HashMap;
 import java.util.NoSuchElementException;
 import java.util.Objects;
 
@@ -37,17 +42,15 @@ public class LoginController {
         ResponseDto responseDto = new ResponseDto();
         try {
             logger.info("========== TRY LOGIN EMAIL:: " + loginDto.getEmail() + " ==========");
-            String encEmail = CommonUtil.encryptAES256(loginDto.getEmail());
-            String encPwd = CommonUtil.encryptSHA256(loginDto.getPwd());
+            String encEmail = EncUtil.encryptAES256(loginDto.getEmail());
+            String encPwd = EncUtil.encryptSHA256(loginDto.getPwd());
             ManagerEntity managerEntity = managerService.findByManagerId(encEmail);
             if (Objects.isNull(managerEntity)) return ResponseUtil.failedAuthentication(responseDto); // ID로 찾을 수 없음.
             responseDto.setData(managerEntity.getLoginFailCnt());
             if (managerEntity.getLoginFailCnt() > 5) return ResponseUtil.tooManyLoginFailedCnt(responseDto); // 로그인 실패 횟수 5회 이상
             if (managerEntity.getManagerPwd().equals(encPwd)) {
                 managerService.updateSuccessLoginStatus(managerEntity);
-                // JWT ?
-                // 일반 문자열 난수 ?
-                responseDto.setData("TOKEN");
+                responseDto.setData(JwtUtil.makeJwt(managerEntity.getManagerSeq()));
                 return ResponseUtil.success(responseDto); // 로그인 성공
             } else {
                 managerService.updateFailLoginStatus(managerEntity);
