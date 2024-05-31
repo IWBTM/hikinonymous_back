9614f14da097989fe905741bbadfb1b4113a75f0
김경민(화려한 덕후들) <gang00@g-oduk.com>
2024-05-31 17:02:24 +0900

    [CMS] interceptor 인증 처리 수정

--- cms/src/main/java/org/hikinonymous/back/cms/interceptor/CmsInterceptor.java
+++ cms/src/main/java/org/hikinonymous/back/cms/interceptor/CmsInterceptor.java
@@ -3,19 +3,38 @@ package org.hikinonymous.back.cms.interceptor;
 import io.micrometer.common.util.StringUtils;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.hikinonymous.back.core.dto.ManagerDto;
+import org.hikinonymous.back.core.entity.ManagerEntity;
+import org.hikinonymous.back.core.service.ManagerService;
+import org.hikinonymous.back.core.utils.CommonUtil;
+import org.hikinonymous.back.core.utils.JwtUtil;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.naming.AuthenticationException;
+import java.util.Objects;
 
 @Component
+@RequiredArgsConstructor
 public class CmsInterceptor implements HandlerInterceptor {
+
+    private final ManagerService managerService;
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         String token = request.getHeader("AccessToken");
         if (StringUtils.isBlank(token)) throw new AuthenticationException();
+
+        long seq = JwtUtil.decJwt(token);
+        if (seq == 0) throw new AuthenticationException();
+
+        ManagerEntity managerEntity = managerService.findByManagerSeq(seq);
+        if (Objects.isNull(managerEntity)) throw new AuthenticationException();
+
+        ManagerDto managerDto = (ManagerDto) CommonUtil.bindToObjectFromObjObject(managerEntity, ManagerDto.class);
+        request.setAttribute("managerDto", managerDto);
         return true;
     }
 
