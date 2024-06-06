4116cf368888544a715dec02819ce21a5b6265c1
IWBTM <rnlduadl2000@naver.com>
2024-06-06 18:48:31 +0900

    [CORE] ResponseDto Swagger 설정

--- core/src/main/java/org/hikinonymous/back/core/dto/ResponseDto.java
+++ core/src/main/java/org/hikinonymous/back/core/dto/ResponseDto.java
@@ -1,14 +1,30 @@
 package org.hikinonymous.back.core.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
+@Schema(
+        description = "응답 DTO"
+)
 @Data
 public class ResponseDto {
 
+    @Schema(
+            description = "응답 상태 코드",
+            example = "200"
+    )
     private int code;
 
+    @Schema(
+            description = "응답 상태 메시지",
+            example = "성공 했습니다."
+    )
     private String message;
 
+    @Schema(
+            description = "응답 데이터",
+            nullable = true
+    )
     private Object data;
 
 }
