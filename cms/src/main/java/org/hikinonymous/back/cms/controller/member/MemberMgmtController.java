edcba064a144899056a8b8faa0d6a8b7aee3e86a
GANG <rnlduadl2000@naver.com>
2024-06-20 00:17:54 +0900

    [CMS] member controller 추가

---
+++ cms/src/main/java/org/hikinonymous/back/cms/controller/member/MemberMgmtController.java
@@ -0,0 +1,110 @@
+package org.hikinonymous.back.cms.controller.member;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.hikinonymous.back.core.dto.BoardDto;
+import org.hikinonymous.back.core.dto.BoardSimpleDto;
+import org.hikinonymous.back.core.dto.ManagerDto;
+import org.hikinonymous.back.core.dto.ResponseDto;
+import org.hikinonymous.back.core.entity.BoardEntity;
+import org.hikinonymous.back.core.service.BoardService;
+import org.hikinonymous.back.core.utils.CommonUtil;
+import org.hikinonymous.back.core.utils.ResponseUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Objects;
+
+@Tag(name = "MEMBER MANAGEMENT MENU", description = "MEMBER MANAGEMENT MENU API DOC")
+@Slf4j
+@RestController
+@RequestMapping(value = "/cms/member/")
+@RequiredArgsConstructor
+public class MemberMgmtController {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    private final BoardService boardService;
+
+    @Operation(
+            summary = "회원 리스트 조회",
+            description = "회원 리스트를 조회한다."
+    )
+    @ApiResponse(
+            description = "응답 에러 코드 DOC 참고"
+    )
+    @GetMapping(value = "{boardType}/list")
+    public ResponseDto list(
+            HttpServletRequest request,
+            @PathVariable @Parameter(
+                    name = "boardType",
+                    description = "회원 타입"
+            ) String boardType
+    ) {
+        ResponseDto responseDto = new ResponseDto();
+        ManagerDto manager = (ManagerDto) request.getAttribute("manager");
+
+        List<BoardEntity> boardEntities = boardService.findAllByBoardType(boardType);
+        responseDto.setData(boardEntities.stream().map(boardEntity ->
+            (BoardSimpleDto) CommonUtil.bindToObjectFromObjObject(boardEntity, BoardSimpleDto.class)
+        ));
+        return ResponseUtil.success(responseDto);
+    }
+
+    @Operation(
+            summary = "회원 상세 조회",
+            description = "회원을 상세 조회한다."
+    )
+    @ApiResponse(
+            description = "응답 에러 코드 DOC 참고"
+    )
+    @GetMapping(value = "{boardType}/view")
+    public ResponseDto view(
+            HttpServletRequest request,
+            @PathVariable @Parameter(
+                    name = "seq",
+                    description = "회원 SEQ"
+            ) Long seq,
+            @PathVariable @Parameter(
+                    name = "boardType",
+                    description = "회원 타입"
+            ) String boardType
+    ) {
+        ResponseDto responseDto = new ResponseDto();
+        ManagerDto manager = (ManagerDto) request.getAttribute("manager");
+
+        responseDto.setData((BoardDto) CommonUtil.bindToObjectFromObjObject(boardService.findById(seq), BoardDto.class));
+        return ResponseUtil.success(responseDto);
+    }
+
+    @Operation(
+            summary = "회원 삭제 여부 수정",
+            description = "회원 삭제 여부를 수정한다."
+    )
+    @ApiResponse(
+            description = "응답 에러 코드 DOC 참고"
+    )
+    @GetMapping(value = "{boardType}/updateDelYn")
+    public ResponseDto updateDelYn(
+            HttpServletRequest request,
+            @RequestBody @Valid BoardDto boardDto
+    ) {
+        ResponseDto responseDto = new ResponseDto();
+        ManagerDto manager = (ManagerDto) request.getAttribute("manager");
+        if (Objects.isNull(manager)) return ResponseUtil.canNotFoundManager(responseDto);
+        if (Objects.isNull(boardDto)) return ResponseUtil.emptyRequestParameter(responseDto);
+
+        CommonUtil.setClientInfo(request, boardDto, manager);
+        boardService.updateDelYn(boardDto);
+        return ResponseUtil.success(responseDto);
+    }
+
+}
